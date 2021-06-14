4f79e9e4d90e6cb81ef5143ce2843dd5f416b421
sulaimon <anuoluwapokorede001@gmail.com>
2021-06-13 23:34:29 -1100

    changed some lines

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import List from './List'
+import Alert from './Alert'
+
+const getLocalStorage = () => {
+  let list = localStorage.getItem('list')
+  if (list) {
+    return JSON.parse(localStorage.getItem('list'))
+  } else {
+    return []
+  }
+}
 
 function App() {
+  const [name, setName] = useState('');
+  const [list, setList] = useState(getLocalStorage());
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: '', 
+  type: ''});
+  const handleSubmit = (e) => {     
+    e.preventDefault();
+    if(!name) {
+      // display alert 
+      showAlert(true,'danger','please enter value')
+    }
+    else if (name && isEditing) {
+      //deal with edit
+      setList(
+        list.map((item) => {
+        if(item.id === editID) {
+          return {...item, title: name }
+        }
+        return item
+      })
+      )
+      setName('')
+      setEditID(null)
+      setIsEditing(false)
+      showAlert(true, 'success', 'value changed')
+    }
+    else {
+      // show alert
+      showAlert(true, 'success', 'task added to the list')
+      const newItem = {id: new Date().getTime().toString(),title: name};
+      setList([...list, newItem]);// list now includes an object newItemy
+      setName('')
+    }
+  }
+
+  const showAlert = (show=false,type="",msg="") => {
+    setAlert({show,type,msg})
+  }
+  const clearList = (show=false,type="",msg="") => {
+    showAlert(true,'danger','empty list')
+    setList([])
+  }
+
+  const removeItem = (id) => {
+    showAlert(true,'danger','task removed')
+    const ritem = list.filter((item) => {
+     return item.id !== id
+    })
+    setList(ritem)
+    //or
+    //setList(list.filter((item) => item.id !== id))
+  }
+
+  const editItem = (id) => {
+    const specificItem = list.find((item) => item.id === id);
+    console.log(specificItem)
+    setIsEditing(true);
+    setEditID(id)
+    setName(specificItem.title)
+  }
+
+
+
+  useEffect(() => {
+    localStorage.setItem('list', JSON.stringify(list))
+  }, [list])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <section className='section-center'>
+        <form className='grocery-form' onSubmit={handleSubmit}>
+          {alert.show && <Alert {...alert} removeAlert={showAlert} list={list} />}
+          <h3>Todo list</h3>
+          <div className="form-control">
+            <input type='text' className='grocery' placeholder='e.g. code' value={name} onChange={(e) => setName(e.target.value)} />
+            <button type='submit' className='submit-btn'>
+              {isEditing ? 'edit' : 'submit'}
+            </button>
+          </div>
+        </form>
+        {list.length > 0 && (
+          <div className='grocery-container'>
+          <List items={list} removeItem={removeItem} editItem={editItem} />
+          <button className='clear-btn' onClick={clearList}>clear list</button>
+        </div>
+        )}
+        </section>
+
   );
 }
 
-export default App;
+export default App
+
+/*
+steps:
+-npm install gh-pages --save-dev
+-git remote add origin repo-url
+-add homepage url
+-add the scripts
+-npm run deploy*/
